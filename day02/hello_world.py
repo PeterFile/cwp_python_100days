3823f3b334ba5cd46b5e7c395197ce93fcbc16c8
PeterFile <1783491278@qq.com>
2025-08-22 21:59:59 +0800

    feat: 完成day02、day03练习

---
+++ day02/hello_world.py
@@ -0,0 +1,8 @@
+"""
+第一个 python 程序
+Version: 1.0
+Author: CWP
+"""
+
+print("hello world!")
+print("goodbye world")
---
+++ day03/var_example.py
@@ -0,0 +1,38 @@
+print(0b100)
+print(0o100)
+print(100)
+print(0x100)
+
+print(123.456)
+print(1.23456e2)
+
+"""
+使用变量保存数据并进行加减乘除
+
+Version: 1.0
+Author: CWP
+"""
+a = 100
+b = 12
+print(a, b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+c = "hello world!"
+d = True
+
+print(type(a))
+print(type(c))
+print(type(d))
+
+e = 1.24
+f = '123'
+g = '1.23'
+
+print(float(a))
+print(int(e))
+print(str(e))
+print(chr(a))
+print(ord('f'))
